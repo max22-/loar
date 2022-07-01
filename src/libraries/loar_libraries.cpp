91026906f532ef933b3749f570daa3b6f229a29b
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-07-01 20:55:49 +0200

    Added an ifdef for SD library

--- src/libraries/loar_libraries.cpp
+++ src/libraries/loar_libraries.cpp
@@ -3,7 +3,11 @@
 #include "SD/loar_sd.h"
 
 int luaopen_loar_libraries(lua_State *L) {
+
+    #ifdef LOAR_USE_SD
     luaL_requiref(L, "SD", luaopen_sd, 1);
     lua_pop(L, 1);
+    #endif
+    
     return 1;
 }
