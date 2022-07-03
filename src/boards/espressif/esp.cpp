7fa8bf3570bbae761bc4397895761d0b85b4edac
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-07-03 16:39:21 +0200

    Added ESP32 wake-up cause

--- src/boards/espressif/esp.cpp
+++ src/boards/espressif/esp.cpp
@@ -2,6 +2,7 @@
 
 #include <Arduino.h>
 #include "lua/lua.hpp"
+#include "loar.h"
 
 static int restart(lua_State *L)
 {
@@ -16,15 +17,37 @@ static int deepSleep(lua_State *L)
     return 0;
 }
 
+static int wakeupCause(lua_State *L)
+{
+    lua_pushinteger(L, esp_sleep_get_wakeup_cause());
+    return 1;
+}
+
 static const struct luaL_Reg esp_lib[] = {
     {"restart", restart},
     {"deepSleep", deepSleep},
+    {"wakeupCause", wakeupCause},
     {NULL, NULL}
 };
 
 int luaopen_esp(lua_State *L)
 {
     luaL_newlib(L, esp_lib);
+    
+    /* Wake-up causes */
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_UNDEFINED);    //!< In case of deep sleep, reset was not caused by exit from deep sleep
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_ALL);          //!< Not a wakeup cause, used to disable all wakeup sources with esp_sleep_disable_wakeup_source
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_EXT0);         //!< Wakeup caused by external signal using RTC_IO
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_EXT1);         //!< Wakeup caused by external signal using RTC_CNTL
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_TIMER);        //!< Wakeup caused by timer
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_TOUCHPAD);     //!< Wakeup caused by touchpad
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_ULP);          //!< Wakeup caused by ULP program
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_GPIO);         //!< Wakeup caused by GPIO (light sleep only)
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_UART);         //!< Wakeup caused by UART (light sleep only)
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_WIFI);              //!< Wakeup caused by WIFI (light sleep only)
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_COCPU);             //!< Wakeup caused by COCPU int
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG);   //!< Wakeup caused by COCPU crash
+    LUA_REGISTER_CONSTANT(ESP_SLEEP_WAKEUP_BT); 
     return 1;
 }
 
